cfeb046c88d7cdfe02384528139df2ec591bd29c
cherjios <slopez0286@gmail.com>
2021-03-15 21:04:22 -0700

    jumbotron to head

--- czdd-consent-forms-app/src/pages/home/index.js
+++ czdd-consent-forms-app/src/pages/home/index.js
@@ -3,9 +3,16 @@ import React from "react";
 
 function Home (){
     return(
-        <div>
-        <h1>Hello</h1>
-        <h2>h2</h2>
+        <div className="container-fluid">
+        <div className="jumbotron">
+  <h1 className="display-2">Cesar Zamora DDS </h1>
+  <h3 className="display-4">Consent Forms</h3>
+    <p className="lead">
+        
+    </p>
+  <p className="lead">
+  </p>
+</div>
 
         </div>
        
