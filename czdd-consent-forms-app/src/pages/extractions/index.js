fc6d1ccdec64f87052b2272e8a01f97da4bad3aa
Sergio Lopez <slopez0286@gmail.com>
2021-07-20 17:35:19 -0700

    Adding jumbo to extractions

--- czdd-consent-forms-app/src/pages/extractions/index.js
+++ czdd-consent-forms-app/src/pages/extractions/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Jumbotron from "../../components/Jumbotron";
 
-function Extractions (){
+function Extractions() {
 
     return (
 
-        <div>
+        <div className="container">
+            <div className="row">
+                <div className="col"> <Jumbotron /></div>
+            </div>
+
             <h1>Extractions</h1>
         </div>
     )
