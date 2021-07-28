76ab8d75bed0f03c1f506d44017a7e385c4b05bb
Sergio Lopez <slopez0286@gmail.com>
2021-07-27 18:07:41 -0700

    Adding more info to extraction page

--- czdd-consent-forms-app/src/pages/extractionsSp/index.js
+++ czdd-consent-forms-app/src/pages/extractionsSp/index.js
@@ -60,31 +60,30 @@ function ExtractionsSp() {
                         <li>Entiendo que el coágulo de sangre necesario que se forma en el alvéolo se puede desintegrar o desprenderse. Esta dolorosa condición llamada alveolitis seca dura una semana o más y se trata colocando un apósito medicinal en la cavidad del diente para ayudar a la curación. Para protegerme contra el desarrollo de una alveolitis seca, no debo fumar, beber con una pajita, enjuagar con agua o enjuague bucal, masticar alimentos en el área o alterar la alveolitis de ninguna manera durante 24 a 48 horas</li>
                         <li>Entiendo que los instrumentos utilizados para extraer un diente pueden inevitablemente astillar o dañar los dientes adyacentes, lo que podría requerir un tratamiento adicional para restaurar su apariencia o función.</li>
                         <li>Entiendo que los dientes superiores tienen raíces que pueden extenderse cerca de los senos nasales. La extracción de estos dientes puede dejar temporalmente una pequeña abertura en los senos nasales. Es posible que se necesiten antibióticos y tratamiento adicional para prevenir la infección de los senos nasales y ayudar a que esta abertura se cierre.</li>
-                        <li>I understand that an extraction may cause a fracture in the surrounding bone. Occasionally, the tooth to be extracted may be fused to the surrounding bone. In both situations, additional treatment is necessary. Bone fragment called “spicules” may arise at the site following extraction and are generally easily remove.</li>
-                        <li> I understand that some tooth fragments might be left behind at the extraction sites depending on the condition and position of the tooth/teeth. In general, this causes no problems, but in rare occasions, the fragment(s) might cause infections and need to be removed.</li>
-                        <li>I understand that the nerves that control sensations in my teeth, gums, tongue, lips and chin run through my jaw. Depending on the tooth to be extracted (particularly lower teeth or third molars), occasionally it may be impossible to avoid touching, moving, stretching, brushing, cutting or severing a nerve. This could change the normal sensations in any of these areas, causing itching, tingling or burning, or the loss of all sensation. These changes could last from several weeks to several months or in some cases, indefinitely.</li>
+                        <li>Entiendo que la extracción puede causar una fractura en el hueso circundante. Ocasionalmente, el diente que se va a extraer puede fusionarse con el hueso circundante. En ambas situaciones, es necesario un tratamiento adicional. Los fragmentos de hueso llamados "espículas" pueden surgir en el sitio después de la extracción y generalmente se eliminan fácilmente.</li>
+                        <li>Entiendo que algunos fragmentos de dientes pueden quedar en los sitios de extracción dependiendo de la condición y posición del diente(s). En general, esto no causa problemas, pero en raras ocasiones, los fragmentos pueden causar infecciones y deben eliminarse.</li>
                     </ol>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    <h3>CONSEQUENCES IF NO TREATMENT ADMINISTERED (Not limited to the following)</h3>
+                    <h3>
+                        CONSECUENCIAS SI NO SE ADMINISTRA EL TRATAMIENTO (No limitado a lo siguiente)
+                    </h3>
                     <ol>
                         <li>
-                            I understand that if no treatment is performed, I may continue to experience symptoms, which could include pain and/or infection, deterioration of the bone surrounding my teeth, changes to my bite, discomfort in my jaw joint, and possibly the premature loss of other teeth.
+                            Entiendo que si no se realiza ningún tratamiento, es posible que continúe experimentando síntomas, que podrían incluir dolor y/o infección, deterioro del hueso que rodea mis dientes, cambios en la mordida, malestar en la articulación de la mandíbula y posiblemente la pérdida prematura de otros dientes.
                         </li>
                     </ol>
-
                 </div>
             </div>
-
             <br />
             <div className="row">
                 <div className="col">
-                    <h3>ALTERNATIVE TREATMENTS IF EXTRACTION NOT THE ONLY SOLUTION  (Not limited to the following)</h3>
+                    <h3>TRATAMIENTOS ALTERNATIVOS SI LA EXTRACCIÓN NO ES LA ÚNICA SOLUCIÓN (No se limita a lo siguiente)</h3>
                     <ol>
                         <li>Entiendo que dependiendo de mi diagnóstico, pueden existir alternativas a la extracción que involucra otras disciplinas de la odontología. Le pregunté a mi dentista sobre ellos y sus respectivos costos. Mis preguntas han sido respondidas a mi satisfacción sobre los procedimientos, sus riesgos, beneficios y costos.</li>
-                        <li>No guarantee or assurance has been given to me by anyone that the prosed treatment or surgery will cure or improve the condition(s) listed above.</li>
+                        <li>Nadie me ha dado ninguna garantía o seguridad de que el tratamiento o la cirugía propuesta curará o mejorarán las condiciones mencionadas anteriormente.</li>
                         <li>By signing this document, I consent to the crown preparation and placement as described above by the doctor, Cesar E. Zamora Perez D.D.S. I attest that I have discussed the risk, benefits, consequences, and alternatives to Crowns treatment with the doctor. I had the opportunity to ask questions, and I understand what has been explained.
                         </li>
                     </ol>
