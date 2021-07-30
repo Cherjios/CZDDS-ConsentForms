5ff8abf011cd0de340e9095cab948524c2c8de13
Sergio Lopez <slopez0286@gmail.com>
2021-07-29 18:29:53 -0700

    Adding deep cleaning consent form

---
+++ czdd-consent-forms-app/src/pages/deep-cleaning/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Jumbotron from "../../components/Jumbotron";
+import Navbar from "../../components/Navbar";
+import Proceets from "../../components/proceets";
+import { Link } from "react-router-dom";
+import "./styles.ccs"
+
