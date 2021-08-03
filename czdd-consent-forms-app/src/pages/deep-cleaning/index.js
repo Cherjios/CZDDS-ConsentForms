7145bdb7e134f5bbeed53d60dd9d9b806a7b4547
Sergio Lopez <slopez0286@gmail.com>
2021-08-02 17:45:34 -0700

    Build-up deep cleaning page

--- czdd-consent-forms-app/src/pages/deep-cleaning/index.js
+++ czdd-consent-forms-app/src/pages/deep-cleaning/index.js
@@ -2,26 +2,42 @@ import React from "react";
 import "./style.css"
 import Navbar from "../../components/Navbar";
 import Jumbotron from "../../components/Jumbotron";
+import { Link } from "react-router-dom";
+import Proceets from "../../components/proceets";
 
 
 
-function Deepcleaning (){
+function Deepcleaning() {
 
-    return(
+    return (
         <div className="container">
-           
-           <div className="row">
-               <div className="col"> <Navbar/> </div>
-           </div>
-           <br/>
 
-           <div className="row">
-               <div className="col"><Jumbotron/></div>
-           </div>
+            <div className="row">
+                <div className="col"> <Navbar /> </div>
+            </div>
+            <br />
+
+            <div className="row">
+                <div className="col"><Jumbotron /></div>
+            </div>
+
+            <div className="textlink1">
+                <Link to="/CZDDS-ConsentForms/deep-cleaning-sp" className="textlink2">Español</Link>
+            </div>
+
+            <div className="row">
+                <div className="col"> <h1 className="title">INFORMED CONSENT FOR DEEP CLEANING</h1></div>
+            </div>
+            <br />
 
 
+            <div className="row">
+                <div className="col">
+                    <Proceets />
+                </div>
 
 
+            </div>
         </div>
     )
 }
