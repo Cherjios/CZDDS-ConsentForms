c8af3df2d97d8317395872d7403f2d7daa8334a4
Sergio Lopez <slopez0286@gmail.com>
2021-08-10 17:22:49 -0700

    Building up root canal sp

--- czdd-consent-forms-app/src/pages/root-canalSp/index.js
+++ czdd-consent-forms-app/src/pages/root-canalSp/index.js
@@ -1,11 +1,36 @@
 import React from "react";
 import "./style.css";
+import Navbar from "../../components/Navbar";
+import Jumbotron from "../../components/Jumbotron";
+import { Link } from "react-router-dom";
 
 
-function RootCanalSp(){
+function RootCanalSp() {
 
-    return(
-        <h1>Root canal</h1>
+    return (
+
+
+        <div className="container">
+
+            <h1>Root canal</h1>
+
+            <div className="row">
+                <div className="col"> <Navbar /></div>
+            </div>
+            <br />
+
+            <div className="row">
+                <div className="col"> <Jumbotron /></div>
+            </div>
+
+            <div className="textlink1">
+            <Link to ="/CZDDS-ConsentForms/root-canal" className="textlink2">English </Link>
+            
+            </div>
+
+        </div>
+
+
     )
 }
 
