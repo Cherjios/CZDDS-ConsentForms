51ae86801da92a46bfae3e1bd11d2da70e6e57df
Sergio Lopez <slopez0286@gmail.com>
2021-08-14 13:33:31 -0700

    Adding header to crown

--- czdd-consent-forms-app/src/pages/crowns/index.js
+++ czdd-consent-forms-app/src/pages/crowns/index.js
@@ -1,11 +1,28 @@
 import React from "react";
+import Jumbotron from "../../components/Jumbotron";
+import Navbar from "../../components/Navbar";
+import Proceets from "../../components/proceets";
+import { Link } from "react-router-dom";
 
 
 function Crowns (){
     return(
 
-        <div>
-            <h1> Crowns</h1>
+        <div className="container">
+           <div className="row">
+               <div className="col"> <Navbar /></div>
+           </div>
+            <br/>
+            
+            <div classNam="row">
+                <div className="col"> <Jumbotron /></div>
+            </div>
+
+            <div className="textlink1">
+            <Link to ="/CZDDS-ConsentForms/crownsSp" className="textlink2">Spanish </Link>
+            </div>
+
+
         </div>
     )
 }
