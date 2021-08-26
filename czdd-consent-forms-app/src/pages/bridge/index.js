dacc012fbb55407ef684ff0f6d3c2c4474df93bc
Sergio Lopez <slopez0286@gmail.com>
2021-08-25 18:01:15 -0700

    Adding link to spanish  form

--- czdd-consent-forms-app/src/pages/bridge/index.js
+++ czdd-consent-forms-app/src/pages/bridge/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Jumbotron from "../../components/Jumbotron";
+import {Link} from "react-router-dom"
 
 function Bridge () {
 
@@ -16,6 +17,11 @@ function Bridge () {
                 <div className="col"> <Jumbotron /></div>
             </div>
 
+            <div className="textlink1">
+                <Link to="/CZDDS-ConsentForms/bridgeSp" className="textlink2">Spanish </Link>
+            </div>
+        
+        
         </div>
     )
 
