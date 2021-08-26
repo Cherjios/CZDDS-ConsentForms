7beeffb04f54d04a0aed9825f68c8b1fe05e8e17
Sergio Lopez <slopez0286@gmail.com>
2021-08-26 16:30:31 -0700

    Adding risks of bridges

--- czdd-consent-forms-app/src/pages/bridge/index.js
+++ czdd-consent-forms-app/src/pages/bridge/index.js
@@ -53,7 +53,24 @@ function Bridge() {
                 <div className="col">
                     <ol>
                         <li>A Bridge is typically used to strengthen a tooth damaged by decay, fracture or previous restorations. It can also serve to protect a tooth that has had a rood canal treatment or improve the way your other teeth fit together.</li>
-                    	<li>Bridges will be used for the purpose of improving the appearance of damaged, discolored, misshapen, and misaligned or poorly spaced teeth.</li>
+                        <li>Bridges will be used for the purpose of improving the appearance of damaged, discolored, misshapen, and misaligned or poorly spaced teeth.</li>
+                    </ol>
+                </div>
+            </div>
+            <br />
+
+            <div className="row">
+                <div className="col"> <h3>RISKS OF BRIDGES (Not limited to the following)</h3></div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <ol>
+                        <li> I understand that preparing a damaged tooth may further irritate the nerve tissue (called the pulp) in the center of the tooth, leaving my tooth feeling sensitive to heat, cold or pressure. Such sensitive teeth may require additional treatment including endodontic or root canal treatment.</li>
+                        <li> I understand that holding my mouth open during treatment may temporarily leave my jaw feeling stiff and sore and may make it difficult for me to open wide for several days. This can occasionally be an indication of a further problem. I must notify your office if this or other concerns arise.</li>
+                        <li> I understand that Bridge may alter the way my teeth fit together and make my jaw joint feel sore. This may require adjusting my bite by altering the biting surface of the Bridge or adjacent teeth.</li>
+                        <li> I understand that the edge of a Bridge is usually near the gum line, which is an area prone to gum irritation, infection or decay. Proper brushing and flossing at home, a healthy diet, and regular professional cleanings are some preventative measures essential to helping control these problems.</li>
+                        <li> I understand that I may receive a local anesthetic and/or other medication. In rare instances patients may have a reaction to the anesthetic, which could require emergency medical attention, or find that it reduces their ability to control swallowing. This increases the normal chance of swallowing foreign objects during treatment. Rarely, temporary or permanent nerve injury can result from an injection.</li>
+                        <li> I understand that all medications have the potential for accompanying risks, side effects, and drug interactions. Therefore, it is critical that I tell my dentist of all medications I am currently taking</li>
                     </ol>
                 </div>
             </div>
