40be4fa1d27a2cae3aec06821edbd3f06146d6f2
Sergio Lopez <slopez0286@gmail.com>
2021-08-26 16:20:29 -0700

    Adding benefit of bridges

--- czdd-consent-forms-app/src/pages/bridge/index.js
+++ czdd-consent-forms-app/src/pages/bridge/index.js
@@ -35,15 +35,20 @@ function Bridge() {
                     <ol>
                         <li>Treatment involves restoring damaged areas of the tooth above and below the gum line with a Bridge.</li>
                         <li>Restoration of a tooth with a Bridge requires two phases:</li>
-                            <lo>
+                            <ol>
                                 <li>Preparation of the tooth, an impression to send to the lab, and construction and temporary cementation of a temporary Bridge: and later,</li>
                                 <li>Removal of the temporary Bridge, adjustment and cementation of the completed Bridge when esthetics and function have been verified. </li>
-                                </lo>
+                                </ol>
                               <li>Once a Temporary Bridge has been placed, it is essential to return to have the new Bridge placed as soon as it is ready because the temporary Bridge is not intended to function as well as the permanent Bridge. Failing to replace the temporary Bridge with a completed one could lead to decay, gym disease, infections, problems with your bite, and even loss of the tooth.</li>
                     </ol>
                 </div>
             </div>
             <br />
+
+            <div className="row">
+                <div className="col"> <h3>BENEFITS OF BRIDGES (Not limited to the following)</h3></div>
+            </div>
+
 
         </div>
     )
