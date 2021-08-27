723250b3f6dddc510a4541b19c20d9b2597f3af9
Sergio Lopez <slopez0286@gmail.com>
2021-08-27 16:27:04 -0700

    Adding navbar to sp bridges page

--- czdd-consent-forms-app/src/pages/bridge-sp/index.js
+++ czdd-consent-forms-app/src/pages/bridge-sp/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Jumbotron from "../../components/Jumbotron";
 import Navbar from "../../components/Navbar";
-import Proceets from "../../components/proceets";
 import {Link} from "react-router-dom"
 
 
 function BridgeSp (){
     return(
         
-        <h1>Puentes</h1>
+        <div className="container">
+            <div className="row"> 
+            <div className="col"><Navbar/>
+
+            </div>
+
+            </div>
+        </div>
 
     )
 }
