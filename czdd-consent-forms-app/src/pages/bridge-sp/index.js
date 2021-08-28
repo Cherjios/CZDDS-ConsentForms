3711edfb13fb4c708f7762932abae3783da8a55f
Sergio Lopez <slopez0286@gmail.com>
2021-08-27 17:36:24 -0700

    Adding more info to sp bridge

--- czdd-consent-forms-app/src/pages/bridge-sp/index.js
+++ czdd-consent-forms-app/src/pages/bridge-sp/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Jumbotron from "../../components/Jumbotron";
 import Navbar from "../../components/Navbar";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 
-function BridgeSp (){
-    return(
-        
+function BridgeSp() {
+    return (
+
         <div className="container">
-            <div className="row"> 
-                <div className="col"><Navbar/></div>
+            <div className="row">
+                <div className="col"><Navbar /></div>
             </div>
-            <br/>
+            <br />
 
             <div className="row">
                 <div className="col"> <Jumbotron />
@@ -27,6 +27,23 @@ function BridgeSp (){
             </div>
             <br />
 
+            <div className="row">
+                <div className="col"> <h3>CONOCIMIENTO DEL PACIENTE </h3></div>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <ol>
+                        <li>El tratamiento involucra la restauración de áreas dañadas del diente, arriba y debajo de la línea de las encías con un puente.</li>
+                        <li> La restauración del diente con un puente tiene dos fases:</li>
+                        <ol>
+                            <li>Preparación del diente, una impresión para enviar al laboratorio, una construcción y cementación temporal de un puente temporal; y luego,</li>
+                            <li>Retiro del puente temporal, ajuste y cementación completado cuando la estética y función han sido verificados. </li>
+                        </ol>	
+                        <li>Una vez que se colocó un puente temporal, es necesario volver para que se coloque el puente nuevo, tan pronto esté listo, porque el puente temporal no fue preparado para funcionar tan bien como el puente permanente. Fallar en reemplazar el puente temporal por el puente permanente, podría llevar al deterioro del diente, enfermedad de encías, infecciones, problemas para masticar y/o hasta la pérdida del diente.</li>
+                    </ol>
+                </div>
+            </div>
 
 
 
