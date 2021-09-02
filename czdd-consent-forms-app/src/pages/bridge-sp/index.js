22dccd4e1074e3bc8f050fb73014cab652ffba9f
Sergio Lopez <slopez0286@gmail.com>
2021-09-02 16:56:57 -0700

    Benefits of bridges in sp page

--- czdd-consent-forms-app/src/pages/bridge-sp/index.js
+++ czdd-consent-forms-app/src/pages/bridge-sp/index.js
@@ -39,12 +39,12 @@ function BridgeSp() {
                         <ol>
                             <li>Preparación del diente, una impresión para enviar al laboratorio, una construcción y cementación temporal de un puente temporal; y luego,</li>
                             <li>Retiro del puente temporal, ajuste y cementación completado cuando la estética y función han sido verificados. </li>
-                        </ol>	
+                        </ol>
                         <li>Una vez que se colocó un puente temporal, es necesario volver para que se coloque el puente nuevo, tan pronto esté listo, porque el puente temporal no fue preparado para funcionar tan bien como el puente permanente. Fallar en reemplazar el puente temporal por el puente permanente, podría llevar al deterioro del diente, enfermedad de encías, infecciones, problemas para masticar y/o hasta la pérdida del diente.</li>
                     </ol>
                 </div>
             </div>
-            <br/>
+            <br />
 
             <div className="row">
                 <div className="col"> <h3>BENEFICIOS DE LOS PUENTES (No limitado a lo siguiente)</h3></div>
@@ -53,7 +53,9 @@ function BridgeSp() {
             <div className="row">
                 <div className="col">
                     <ol>
+                        <li>Los puentes dentales cubren literalmente los espacios creados por uno o más dientes faltantes.</li>
                         <li>Un puente se usa típicamente para fortalecer un diente dañado por el deterioro, fractura o restauraciones previas. También sirve para proteger un diente que ha tenido un tratamiento de conducto de raíz o mejorar la manera en que sus dientes encajan.  </li>
+                        <li>Los puentes se usarán con el propósito de mejorar la apariencia de dientes dañados, decolorados, mal formados, desalineados o pobremente espaciados.</li>
                     </ol>
                 </div>
             </div>
