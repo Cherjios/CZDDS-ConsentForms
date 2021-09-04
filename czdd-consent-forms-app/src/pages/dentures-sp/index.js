38228bb917539e985083d366ee9157bd394a14c0
Sergio Lopez <slopez0286@gmail.com>
2021-09-03 17:48:54 -0700

    Adding  fuction denturesSp

---
+++ czdd-consent-forms-app/src/pages/dentures-sp/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Jumbotron from "../../components/Jumbotron";
+import Navbar from "../../components/Navbar";
+import {Llink } from "react-router-dom"
+import SPproceets from "../../components/SPproceets";
+
+function DenturesSp (){
+    return(
+
+        <div>
+            <h1>Dentaduras</h1>
+        </div>
+    )
+}
+export default DenturesSp;
