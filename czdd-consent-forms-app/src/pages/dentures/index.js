2d9e162835b7bf19845303eee3b1f811499ccef0
Sergio Lopez <slopez0286@gmail.com>
2021-09-03 17:45:35 -0700

    Adding denture function

---
+++ czdd-consent-forms-app/src/pages/dentures/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Jumbotron from "../../components/Jumbotron";
+import Navbar from "../../components/Navbar";
+import Proceets from "../../components/proceets";
+import { Link } from "react-router-dom";
+
+function Dentures (){
+    return(
+        <div>
+            <h3>Dentures</h3>
+        </div>
+    )
+}
+
+export default Dentures;
