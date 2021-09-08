708d35b34f36aae96a4493d01b5e735724289492
Sergio Lopez <slopez0286@gmail.com>
2021-09-08 16:38:10 -0700

    Adding conocimiento del paciente

--- czdd-consent-forms-app/src/pages/dentures-sp/index.js
+++ czdd-consent-forms-app/src/pages/dentures-sp/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Jumbotron from "../../components/Jumbotron";
 import Navbar from "../../components/Navbar";
-import {Llink } from "react-router-dom"
+import { Llink } from "react-router-dom"
 import SPproceets from "../../components/SPproceets";
 
-function DenturesSp (){
-    return(
+function DenturesSp() {
+    return (
 
         <div className="container">
             <div className="row">
                 <div className="col"><Navbar /></div>
             </div>
-            <br/>
+            <br />
 
             <div className="row">
                 <div className="col"><Jumbotron />
@@ -28,6 +28,27 @@ function DenturesSp (){
             <div className="row">
                 <div className="col"> <h3>CONOCIMIENTO DEL PACIENTE </h3></div>
             </div>
+            <div className="row">
+                <div className="col">
+                    <ol>
+
+                        <li>Las dentaduras completas/parciales, también llamadas Prostodoncia, es reemplazar la falta de dientes con dientes artificiales. El tratamiento involucra tomar impresiones de la boca para poder fabricar la dentadura en un laboratorio dental.</li>
+                        <li>Las dentaduras completas están enteramente soportadas por los tejidos (membrana mucosas, tejidos conectivos y hueso subyacentes).</li>
+                        <li>Las dentaduras parciales removibles podrían estar soportadas enteramente por los dientes, o podrían estar soportadas por dientes y tejidos del borde residual.</li>
+                        <li>Las dentaduras completas/parciales requieren un mínimo de cinco vistas y podrían requerir varios ajustes después de la inserción. Estos ajustes no tendrán costo por un periodo de seis meses después de la inserción.</li>
+
+
+                    </ol>
+                </div>
+            </div>
+            <br />
+
+
+
+
+
+
+
 
         </div>
     )
