af111967533f32c91aa7ab2b51a51e73b72df225
Sergio Lopez <slopez0286@gmail.com>
2021-09-08 16:50:49 -0700

    Adding riesgos e dentaduras

--- czdd-consent-forms-app/src/pages/dentures-sp/index.js
+++ czdd-consent-forms-app/src/pages/dentures-sp/index.js
@@ -37,6 +37,35 @@ function DenturesSp() {
                         <li>Las dentaduras parciales removibles podrían estar soportadas enteramente por los dientes, o podrían estar soportadas por dientes y tejidos del borde residual.</li>
                         <li>Las dentaduras completas/parciales requieren un mínimo de cinco vistas y podrían requerir varios ajustes después de la inserción. Estos ajustes no tendrán costo por un periodo de seis meses después de la inserción.</li>
 
+                    </ol>
+                </div>
+            </div>
+            <br />
+
+            <div className="row">
+                <div className="col"> <h3>BENEFICIOS DE LAS DENTADURAS (No limitado a lo siguiente)</h3></div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <ol>
+                        <li> La fabricación de dentaduras completas/parciales tiene la intención de permitirle restaurar la función de masticar, y mejorar su estética, que a su vez, ayudarán a mantener su mordida natural y preservar las estructuras orales remanentes. </li>
+                    </ol>
+                </div>
+            </div>
+            <br />
+
+            <div className="row">
+                <div className="col"> <h3>RIESGOS DE LAS DENTADURAS (No limitado a lo siguiente)</h3></div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <ol>
+                        	<li>Entiendo que después el tratamiento podría experimentar dificultad para hablar por varios días o semanas, lo cual eventualmente mejorará.</li>
+                        	<li>Entiendo que podría experimentar dificultad para masticar. Se necesita un cierto periodo de adaptación antes que al comer sea cómodo.</li>
+                        	<li>Entiendo que las dentaduras completas/parciales podrían tener problemas de retención que pueden necesitar el uso de adhesivos.</li>
+                        	<li>Entiendo que podría ocurrir irritación mucosa y requerir ajustes. Entiendo que debo de informar a mi dentista si se presenta dolor.</li>
+                        	<li>Entiendo que ocasionalmente no se eliminarán totalmente dobleces y arrugas alrededor de la boca con las dentaduras completas/parciales.</li>
+                        	<li>Entiendo que ocasionalmente podría ocurrir lo siguiente: dificultad para tragar, alimentos bajo de las dentaduras completas/parciales, saliva debajo de las dentaduras completas/parciales, pérdida de la sensación de sabor, dislocación de la dentadura completa/parcial al estornudo o beber agua, mordida de mejillas, labios o lengua y/o atragantamiento. La mayoría de estos son raros y pueden manejarse con adaptación al uso de dentaduras completas/parciales.</li>
 
                     </ol>
                 </div>
