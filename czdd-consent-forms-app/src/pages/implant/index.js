8b875bd9fb251b8c24bbc0e97bd6d216558610ac
Sergio Lopez <slopez0286@gmail.com>
2021-09-29 17:35:15 -0700

    Adding jumbotron to implants

--- czdd-consent-forms-app/src/pages/implant/index.js
+++ czdd-consent-forms-app/src/pages/implant/index.js
@@ -1,10 +1,25 @@
 import React from "react"
+import Jumbotron from "../../components/Jumbotron"
+import Navbar from "../../components/Navbar"
+
 
 function Implant (){
     return(
-        <div>
-            <h1>Implnat</h1>
+        <div className="container">
+
+            <div className="row">
+                <div className="col"><Navbar /></div>
+            </div>
+            <br />
+
+            <div className="row">
+                <div className="col"> <Jumbotron /> </div>
+            </div>
+
+
         </div>
+
+
     )
 }
 
