b0172234555339ae51747546eab8db9c8cfc173c
Sergio Lopez <slopez0286@gmail.com>
2021-09-29 17:43:51 -0700

    Adding link to spanish page

--- czdd-consent-forms-app/src/pages/implant/index.js
+++ czdd-consent-forms-app/src/pages/implant/index.js
@@ -1,6 +1,8 @@
-import React from "react"
-import Jumbotron from "../../components/Jumbotron"
-import Navbar from "../../components/Navbar"
+import React from "react";
+import Jumbotron from "../../components/Jumbotron";
+import Navbar from "../../components/Navbar";
+import {Link} from "react-router-dom"
+
 
 
 function Implant (){
@@ -14,6 +16,10 @@ function Implant (){
 
             <div className="row">
                 <div className="col"> <Jumbotron /> </div>
+            </div>
+
+            <div className="textlink1">
+            <Link to ="/CZDSS-ConsentForms/implantSp" className="textlink2">Spanish </Link>
             </div>
 
 
