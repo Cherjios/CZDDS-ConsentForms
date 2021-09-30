8e1425dce71f86d84669bb8548e1b3dc264b8307
Sergio Lopez <slopez0286@gmail.com>
2021-09-30 14:26:48 -0700

    Adding info to sp implant

--- czdd-consent-forms-app/src/pages/implantSp/index.js
+++ czdd-consent-forms-app/src/pages/implantSp/index.js
@@ -1,9 +1,27 @@
-import React from "react"
+import React from "react";
+import Jumbotron from "../../components/Jumbotron";
+import Navbar from "../../components/Navbar";
+import SPproceets from "../../components/SPproceets";
+import {Link}   from "react-router-dom";
+import Footer from "../../components/Footer";
+
 
 function Implantsp (){
     return(
-        <div>
-            <h1>Implnates</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col"><Navbar /></div>
+            </div>
+            <br/>
+
+            <div className="row">
+                <div className="col"> <Jumbotron /></div>
+            </div>
+
+            <div className="textlink1">
+                <Link to="/CZDSS-ConsentForms/implant" className="textlink2">English </Link>
+            </div>
+
         </div>
     )
 }
