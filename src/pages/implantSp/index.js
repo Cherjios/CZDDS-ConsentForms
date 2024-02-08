4ea3cb2416e4a90c78b2604f28a4be95095c0ec4
Sergio Lopez <slopez0286@gmail.com>
2024-02-07 16:43:46 -0800

    removing unused codes

--- src/pages/implantSp/index.js
+++ src/pages/implantSp/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import Jumbotron from "../../components/Jumbotron";
 import Navbar from "../../components/Navbar";
-import SPproceets from "../../components/SPproceets";
 import {Link}   from "react-router-dom";
-import Footer from "../../components/Footer";
-
 
 function Implantsp (){
     return(
